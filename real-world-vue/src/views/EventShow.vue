1ba5e86e3e2a106f398c6a101900f2c5459e9085
Marius Ileana <marius.ileana@connectis.nl>
2020-03-16 20:25:19 +0200

    rwv > Dynamic Segment in a Route

--- real-world-vue/src/views/EventShow.vue
+++ real-world-vue/src/views/EventShow.vue
@@ -1,3 +1,10 @@
 <template>
-  <h1>Showing Event #1</h1>
+  <h1>Showing Event #{{ id }}</h1>
 </template>
+
+// tell the component that it expects a single prop named `id`
+<script>
+export default {
+  props: ['id']
+}
+</script>
