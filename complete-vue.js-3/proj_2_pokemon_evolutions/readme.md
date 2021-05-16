f84c74016b0d07f40ca0bacce10f098b59015378
dxps <marius.ileana@gmail.com>
2021-05-16 17:23:31 +0300

    [complete-vuejs3] proj_2: card div, fetch remote data

---
+++ complete-vue.js-3/proj_2_pokemon_evolutions/readme.md
@@ -0,0 +1,16 @@
+## Pokemon Evolutions
+
+This project showcases:
+
+- creating and using custom and reusable components
+- fetching remote data using Fetch API (from https://pokeapi.co)
+
+### Run
+
+As prerequisites, install the dependencies using `yarn`.
+
+To start in development mode use `yarn vite`.
+
+### Notes
+
+This project skeleton was creating using `yarn create @vitejs/app`.
--- complete-vue.js-3/proj_2_pokemon_evolutions/src/App.vue
+++ complete-vue.js-3/proj_2_pokemon_evolutions/src/App.vue
@@ -1,22 +1,65 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+    <div class="card">
+        <button @click="fetchData">Fetch</button>
+        <div class="title">title</div>
+        <div class="content">content</div>
+        <div class="description">desc</div>
+    </div>
 </template>
 
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
 
-// This starter template is using Vue 3 experimental <script setup> SFCs
-// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+
+<script>
+const api = "https://pokeapi.co/api/v2/pokemon";
+
+export default {
+    methods: {
+        async fetchData() {
+            const response = await window.fetch(`${api}/1`);
+            const data = await response.json();
+            const pokemon = {
+                name: data.name,
+                sprite: data.sprites.other["official-artwork"],
+                types: data.types.map((type) => ({ name: type.type.name })),
+            };
+            console.log("pokemon:", pokemon);
+        },
+    },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+
+<style scoped>
+.card {
+    border: 1px solid silver;
+    border-radius: 8px;
+    max-width: 200px;
+    margin: 0 5px;
+    cursor: pointer;
+    box-shadow: 0px 1px 3px darkgrey;
+    transition: 0.2s;
+}
+
+.title,
+.content,
+.description {
+    padding: 16px;
+    text-transform: capitalize;
+    text-align: center;
+}
+
+.title,
+.content {
+    border-bottom: 1px solid silver;
+}
+
+.title {
+    font-size: 1.25em;
+}
+
+.card:hover {
+    transition: 0.2s;
+    box-shadow: 0px 1px 9px darkgrey;
 }
 </style>
