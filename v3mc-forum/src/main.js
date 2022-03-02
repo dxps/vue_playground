eab26644495742c9573aafa740dc54b1643e6416
dxps <marius.ileana@gmail.com>
2022-03-02 19:58:19 +0200

    v3mc-forum: dyn routing, params & own config

--- v3mc-forum/src/main.js
+++ v3mc-forum/src/main.js
@@ -1,18 +1,7 @@
 import { createApp } from "vue"
-import { createRouter, createWebHistory } from "vue-router"
+import router from "@/router"
 import App from "./App.vue"
-import PageHome from "@/components/PageHome"
-
-const routes = [
-    { path: "/", name: "Home", component: PageHome }
-]
-
-const router = createRouter({
-    history: createWebHistory(),
-    routes
-})
 
 const app = createApp(App)
-
 app.use(router)
 app.mount("#app")
