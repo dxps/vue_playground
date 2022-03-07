827a35e12e779fdaf7004d3f4bfaa14be05d669c
dxps <marius.ileana@gmail.com>
2022-03-07 14:13:20 +0200

    v3mc-forum: PostList w/ human readable & relative date

--- v3mc-forum/src/components/PostList.vue
+++ v3mc-forum/src/components/PostList.vue
@@ -17,8 +17,8 @@
                 </div>
             </div>
 
-            <div class="post-date text-faded">
-                {{ post.publishedAt }}
+            <div class="post-date text-faded" :title="humanFriendlyDate(post.publishedAt)">
+                {{ diffForHumans(post.publishedAt) }}
             </div>
         </div>
     </div>
@@ -26,6 +26,12 @@
 
 <script>
 import srcData from "@/data.json"
+import dayjs from "dayjs"
+import relativeTime from "dayjs/plugin/relativeTime"
+import localizedDate from "dayjs/plugin/localizedFormat"
+
+dayjs.extend(relativeTime)
+dayjs.extend(localizedDate)
 
 export default {
     props: {
@@ -40,6 +46,12 @@ export default {
     methods: {
         userById(userId) {
             return this.users.find((p) => p.id === userId)
+        },
+        diffForHumans(timestamp) {
+            return dayjs.unix(timestamp).fromNow()
+        },
+        humanFriendlyDate(timestamp) {
+            return dayjs.unix(timestamp).format("llll")
         }
     }
 }
