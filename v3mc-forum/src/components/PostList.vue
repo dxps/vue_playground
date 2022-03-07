43a82b2106f6f317047094620a9fa41f24aaf53d
dxps <marius.ileana@gmail.com>
2022-03-07 14:45:39 +0200

    v3mc-forum: AppDate as global comp

--- v3mc-forum/src/components/PostList.vue
+++ v3mc-forum/src/components/PostList.vue
@@ -17,8 +17,8 @@
                 </div>
             </div>
 
-            <div class="post-date text-faded" :title="humanFriendlyDate(post.publishedAt)">
-                {{ diffForHumans(post.publishedAt) }}
+            <div class="post-date text-faded">
+                <app-date :timestamp="post.publishedAt" />
             </div>
         </div>
     </div>
@@ -26,12 +26,6 @@
 
 <script>
 import srcData from "@/data.json"
-import dayjs from "dayjs"
-import relativeTime from "dayjs/plugin/relativeTime"
-import localizedDate from "dayjs/plugin/localizedFormat"
-
-dayjs.extend(relativeTime)
-dayjs.extend(localizedDate)
 
 export default {
     props: {
@@ -46,12 +40,6 @@ export default {
     methods: {
         userById(userId) {
             return this.users.find((p) => p.id === userId)
-        },
-        diffForHumans(timestamp) {
-            return dayjs.unix(timestamp).fromNow()
-        },
-        humanFriendlyDate(timestamp) {
-            return dayjs.unix(timestamp).format("llll")
         }
     }
 }
--- v3mc-forum/src/main.js
+++ v3mc-forum/src/main.js
@@ -4,4 +4,19 @@ import App from "./App.vue"
 
 const app = createApp(App)
 app.use(router)
+
+// Registering global components automatically,
+// based on the "App" prefix name.
+const requireComponent = require.context(
+    "./components",
+    true,
+    /App[A-Z]\w+\.(vue|js)$/
+)
+requireComponent.keys().forEach(function (fileName) {
+    let baseComponentConfig = requireComponent(fileName)
+    baseComponentConfig = baseComponentConfig.default || baseComponentConfig
+    const baseComponentName = baseComponentConfig.name || fileName.replace(/^.+\//, "").replace(/\.\w+$/, "")
+    app.component(baseComponentName, baseComponentConfig)
+})
+
 app.mount("#app")
