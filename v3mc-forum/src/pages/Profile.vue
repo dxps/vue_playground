36df27fef417d25d8f657a102e6ba35d51b166b2
dxps <marius.ileana@gmail.com>
2022-03-08 20:21:35 +0200

    v3mc-forum: user profile view & edit

--- v3mc-forum/src/pages/Profile.vue
+++ v3mc-forum/src/pages/Profile.vue
@@ -2,37 +2,8 @@
     <div class="container">
         <div class="flex-grid">
             <div class="col-3 push-top">
-                <div class="profile-card">
-                    <p class="text-center">
-                        <img :src="user.avatar" :alt="`${user.name} profile picture`" class="avatar-xlarge" />
-                    </p>
-
-                    <h1 class="title">{{ user.username }}</h1>
-
-                    <p class="text-lead">{{ user.name }}</p>
-
-                    <p class="text-justify">{{ user.bio || "No bio specified." }}</p>
-
-                    <span class="online">{{ user.username }} is online</span>
-
-                    <div class="stats">
-                        <span>{{ userPostsCount }} posts</span>
-                        <span>{{ userThreadsCount }} threads</span>
-                    </div>
-
-                    <hr />
-
-                    <p v-if="user.website" class="text-large text-center">
-                        <i class="fa fa-globe"></i> <a :href="user.website">{{ user.website }}</a>
-                    </p>
-                </div>
-
-                <p class="text-xsmall text-faded text-center">Member since june 2003, last visited 4 hours ago</p>
-
-                <div class="text-center">
-                    <hr />
-                    <a href="edit-profile.html" class="btn-green btn-small">Edit Profile</a>
-                </div>
+                <user-profile-card v-if="!edit" :user="user" />
+                <user-profile-card-editor v-else :user="user" />
             </div>
 
             <div class="col-7 push-top">
@@ -43,7 +14,7 @@
 
                 <hr />
 
-                <post-list :posts="userPosts" />
+                <post-list :posts="user.posts" />
             </div>
         </div>
     </div>
@@ -52,26 +23,16 @@
 <script>
 import PostList from "@/components/PostList.vue"
 import { mapGetters } from "vuex"
+import UserProfileCard from "../components/UserProfileCard.vue"
+import UserProfileCardEditor from "../components/UserProfileCardEditor.vue"
 
 export default {
-    computed: {
-        ...mapGetters({ user: "authUser" }),
-        userPosts() {
-            return this.$store.state.posts.filter((post) => post.userId === this.user.id)
-        },
-        userPostsCount() {
-            return this.userPosts.length
-        },
-        userThreads() {
-            return this.$store.state.threads.filter((thread) => thread.userId === this.user.id)
-        },
-        userThreadsCount() {
-            return this.userThreads.length
-        }
+    props: {
+        edit: { default: false, type: Boolean }
     },
-    components: { PostList }
+    computed: {
+        ...mapGetters({ user: "authUser" })
+    },
+    components: { UserProfileCard, UserProfileCardEditor, PostList }
 }
 </script>
-
-<style>
-</style>
--- v3mc-forum/src/store/index.js
+++ v3mc-forum/src/store/index.js
@@ -7,13 +7,34 @@ export default createStore({
         authId: "ALXhxjwgY9PinwNGHpfai6OWyDu2"
     },
     getters: {
-        authUser: state => state.users.find(user => user.id === state.authId)
+        authUser: state => {
+            const user = state.users.find(user => user.id === state.authId)
+            if (!user) return null
+            return {
+                ...user,
+                get posts() {
+                    return state.posts.filter((post) => post.userId === user.id)
+                },
+                get postsCount() {
+                    return this.posts.length
+                },
+                get threads() {
+                    return state.threads.filter((thread) => thread.userId === user.id)
+                },
+                get threadsCount() {
+                    return this.threads.length
+                }
+            }
+        }
     },
     actions: {
         createPost(context, post) {
             post.id = "temp-dev-" + Math.random()
             context.commit("addPost", { post })
             context.commit("addPostRefToThread", { postId: post.id, threadId: post.threadId })
+        },
+        updateUserProfile({ commit }, user) {
+            commit("setUserProfile", { user, userId: user.id })
         }
     },
     mutations: {
@@ -23,6 +44,10 @@ export default createStore({
         addPostRefToThread(state, { postId, threadId }) {
             const thread = state.threads.find(thread => thread.id === threadId)
             thread.posts.push(postId)
+        },
+        setUserProfile(state, { user, userId }) {
+            const userIdx = state.users.findIndex(user => user.id === userId)
+            state.users[userIdx] = user
         }
     }
 })
