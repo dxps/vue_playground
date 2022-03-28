c9b1097f377afa718f9d61a6b31de22d123c654b
dxps <marius.ileana@gmail.com>
2022-03-28 23:16:05 +0300

    pinia-pineapple-stand: 1st commit

---
+++ pinia-pineapple-stand/README.md
@@ -0,0 +1,10 @@
+# pinia-pineapple-stand
+
+This sample is based on [this course](https://vueschool.io/courses/pinia-the-enjoyable-vue-store).
+
+## How to Use
+
+```
+npm install
+npm run dev
+```
---
+++ pinia-pineapple-stand/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import TheHeader from "@/components/TheHeader.vue"
+import ProductCard from "@/components/ProductCard.vue"
+import { useProductStore } from "./stores/ProductStore"
+
+// Destructuring, but having references to reactive props
+// in the pinia store state. Useful for just accessing
+// data from the state, but not calling any action.
+// import { storeToRefs } from "pinia"
+// const { products } = storeToRefs(useProductStore())
+
+const productStore = useProductStore()
+productStore.fill()
+</script>
+
+<template>
+	<div class="container">
+		<TheHeader />
+		<ul class="sm:flex flex-wrap lg:flex-nowrap gap-5">
+			<ProductCard v-for="product in productStore.products" :key="product.name" :product="product" />
+		</ul>
+	</div>
+</template>
