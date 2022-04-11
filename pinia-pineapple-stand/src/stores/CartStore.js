e6121eb20f84f9f60c38fea0bf1f51b03a987fdf
dxps <marius.ileana@gmail.com>
2022-04-11 20:28:25 +0300

    feat(pinia-pineapple-stand): cart items, total, clear

--- pinia-pineapple-stand/src/stores/CartStore.js
+++ pinia-pineapple-stand/src/stores/CartStore.js
@@ -1,10 +1,19 @@
 import { defineStore } from "pinia";
+import { groupBy } from "lodash";
 
 export const useCartStore = defineStore("CartStore", {
   state: () => {
     return {
       items: [],
     }
+  },
+  getters: {
+    count: (state) => state.items.length,
+    isEmpty: (state) => state.count === 0,
+    grouped: (state) => groupBy(state.items, (item) => item.name),
+    // This 'groupCount' is like a "dynamic getter".
+    groupCount: (state) => (name) => state.grouped[name].length,
+    totalPrice: (state) => state.items.reduce((prev, curr) => prev + curr.price, 0)
   },
   actions: {
     addItems(count, item) {
@@ -14,7 +23,9 @@ export const useCartStore = defineStore("CartStore", {
         // instead of passing it by reference.
         this.items.push({ ...item })
       }
+    },
+    removeItem(itemName) {
+      this.items = this.items.filter((elem) => item.name !== itemName)
     }
   }
 });
-
--- pinia-pineapple-stand/src/stores/ProductStore.js
+++ pinia-pineapple-stand/src/stores/ProductStore.js
@@ -25,5 +25,4 @@ export const useProductStore = defineStore("ProductStore", {
   // -------
 
 
-
 })
