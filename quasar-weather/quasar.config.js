5e5440947ba8e03cc0bc414e950856f054e0e29d
dxps <marius.ileana@gmail.com>
2022-06-21 14:56:17 +0300

    feat(quasar-weather): layout & design

--- quasar-weather/quasar.config.js
+++ quasar-weather/quasar.config.js
@@ -13,7 +13,9 @@ const { configure } = require('quasar/wrappers');
 const path = require('path');
 
 module.exports = configure(function (/* ctx */) {
+
   return {
+
     eslint: {
       // fix: true,
       // include = [],
@@ -56,7 +58,7 @@ module.exports = configure(function (/* ctx */) {
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#build
     build: {
       target: {
-        browser: [ 'es2019', 'edge88', 'firefox78', 'chrome87', 'safari13.1' ],
+        browser: ['es2019', 'edge88', 'firefox78', 'chrome87', 'safari13.1'],
         node: 'node16'
       },
 
@@ -93,7 +95,7 @@ module.exports = configure(function (/* ctx */) {
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#devServer
     devServer: {
       // https: true
-      open: true // opens browser window automatically
+      open: false // opens browser window automatically
     },
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#framework
@@ -133,7 +135,7 @@ module.exports = configure(function (/* ctx */) {
     // https://v2.quasar.dev/quasar-cli-vite/developing-ssr/configuring-ssr
     ssr: {
       // ssrPwaHtmlFilename: 'offline.html', // do NOT use index.html as name!
-                                          // will mess up SSR
+      // will mess up SSR
 
       // extendSSRWebserverConf (esbuildConf) {},
       // extendPackageJson (json) {},
@@ -144,7 +146,7 @@ module.exports = configure(function (/* ctx */) {
       // manualPostHydrationTrigger: true,
 
       prodPort: 3000, // The default port that the production server should use
-                      // (gets superseded if process.env.PORT is specified at runtime)
+      // (gets superseded if process.env.PORT is specified at runtime)
 
       middlewares: [
         'render' // keep this as last one
--- quasar-weather/src/pages/IndexPage.vue
+++ quasar-weather/src/pages/IndexPage.vue
@@ -1,43 +1,63 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example Component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
+  <q-page class="flx column">
+    <div class="col q-pt-lg q-px-md">
+      <q-input v-model="search" Placehoder="Search" dark borderless>
+        <template v-slot:before>
+          <q-icon name="my_location" />
+        </template>
+
+        <template v-slot:append>
+          <q-btn round dense flat icon="search" />
+        </template>
+      </q-input>
+    </div>
+
+    <template v-if="weatherData">
+      <div class="col text-white text-center">
+        <div class="text-h4 text-weight-ligh">Bucharest</div>
+        <div class="text-h6">Sunny</div>
+        <div class="text-h1 text-weight-thin q-my-lg relative-position">
+          <span>32</span>
+          <span class="text-h4 relative-position degree">&deg;</span>
+        </div>
+      </div>
+
+      <div class="col text-center">
+        <img src="https://www.fillmurray.com/100/100" />
+      </div>
+    </template>
+    <template v-else>
+      <div class="col column text-center text-white">
+        <div class="col text-h2 text-weight-thin">Quasar<br />Weather</div>
+        <q-btn class="col" flat>
+          <q-icon left size="3em" name="my_location" />
+          <div>Find my location</div>
+        </q-btn>
+      </div>
+    </template>
+
+    <div class="col skyline"></div>
   </q-page>
 </template>
 
 <script setup lang="ts">
-import { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/ExampleComponent.vue';
 import { ref } from 'vue';
 
-const todos = ref<Todo[]>([
-  {
-    id: 1,
-    content: 'todo 1',
-  },
-  {
-    id: 2,
-    content: 'todo 2',
-  },
-  {
-    id: 3,
-    content: 'todo 3',
-  },
-  {
-    id: 4,
-    content: 'todo 4',
-  },
-  {
-    id: 5,
-    content: 'todo 5',
-  },
-]);
+let search = ref('');
 
-const meta = ref<Meta>({
-  totalCount: 1000,
-});
+let weatherData = ref(null);
 </script>
+
+<style lang="sass">
+.q-page
+  background: linear-gradient(to bottom, #136a8a, #267871)
+
+.degree
+  top: -44px
+
+.skyline
+  flex: 0 0 100px
+  background: url(/skyline.png)
+  background-size: contain
+  background-position:center bottom
+</style>
