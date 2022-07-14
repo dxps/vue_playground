1f4fd97936cff45d89182660affbf30d646062ed
dxps <marius.ileana@gmail.com>
2022-07-14 19:32:12 +0300

    vue-i18n-ex1: minimal working example

---
+++ vue-i18n-ex1/readme.md
@@ -0,0 +1,26 @@
+# vue-i18n-ex1
+
+A first example using `vue-i18n` with a Vue 3 app.
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn serve
+```
+
+### Compiles and minifies for production
+```
+yarn build
+```
+
+### Lints and fixes files
+```
+yarn lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ vue-i18n-ex1/src/components/HelloI18n.vue
@@ -0,0 +1,21 @@
+<template>
+	<p>message: {{ $t('message') }}</p>
+	<p>goodbye: {{ $t('goodbye') }}</p>
+	<button @click="setLocale('en')" class="button">English</button>
+	<button @click="setLocale('es')" class="button">Spanish</button>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+// import { useI18n } from 'vue-i18n'
+
+export default defineComponent({
+	name: 'HelloI18n',
+	methods: {
+		setLocale(locale) {
+			console.log("Setting locale to '" + locale + "'.")
+			this.$i18n.locale = locale
+		},
+	},
+})
+</script>
