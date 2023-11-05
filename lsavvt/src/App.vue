58ddde27bf342c1ac7ac9ad84c970495ccd3a46f
dxps <marius.ileana@gmail.com>
2023-11-05 21:42:49 +0200

    lsavvt project added

---
+++ lsavvt/src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import ItemsListComp from './components/ItemsList.comp.vue'
+
+// Mocking some data.
+const items: any[] = [
+	{ id: 1, name: 'Item 1' },
+	{ id: 2, name: 'Item 2' },
+	{ id: 3, name: 'Item 3' },
+]
+</script>
+
+<template>
+	<div class="home">
+		<ItemsListComp :items="items" />
+	</div>
+</template>
+
+<style scoped></style>
---
+++ lsavvt/src/components/ItemsList.comp.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+defineProps<{ items: any[] }>()
+</script>
+
+<template>
+	<div>
+		<h3>Items:</h3>
+		<ul>
+			<li v-for="(item, idx) in items" :key="item.id">{{ item.name }}</li>
+		</ul>
+	</div>
+</template>
